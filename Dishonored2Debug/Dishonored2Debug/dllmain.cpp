84307d92dea2d6e8a26f312df9b33d310202b87b
Radai <lordradai@null.net>
2025-08-19 17:04:55 +0200

    fix

--- Dishonored2Debug/Dishonored2Debug/dllmain.cpp
+++ Dishonored2Debug/Dishonored2Debug/dllmain.cpp
@@ -74,6 +74,15 @@ bool BindConsole()
         SetStdHandle(STD_OUTPUT_HANDLE, GetStdHandle(STD_OUTPUT_HANDLE));
         SetStdHandle(STD_ERROR_HANDLE, GetStdHandle(STD_ERROR_HANDLE));
 
+        HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+        if (hOut == INVALID_HANDLE_VALUE) return false;
+
+        DWORD dwMode = 0;
+        if (!GetConsoleMode(hOut, &dwMode)) return false;
+
+        dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+        SetConsoleMode(hOut, dwMode);
+
         return true;
     }
 
